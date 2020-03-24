4ae9801befbdc013e000472f5c2b2b7293a1aa76
therealjustindude <therealjustindude@gmail.com>
2020-03-24 16:55:47 -0500

    Getting the name and id attributes from the API

--- lib/CLIproject/api.rb
+++ lib/CLIproject/api.rb
@@ -5,28 +5,10 @@ class API
 
     def self.tequila_drinks
         response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=tequila")
-       
         response["drinks"].each do |arr|
             arr.reject! {|attr| attr == "strDrinkThumb"}
-            arr.each do |k,v|
-                hash={}
-                hash[k.to_sym] = v
-                
-                
-                
-                TequilaDrink.new(hash)
-                # drink_name=""
-                # drink_id=0
-                # case k
-                # when "strDrink"
-                #     drink_name= v
-                # when "idDrink"
-                #     drink_id = v
-                # end
-            end
-            binding.pry
+                TequilaDrink.new(arr)
         end
-       
     end
 
     # def self.tequila_by_id
--- lib/CLIproject/tequila_drink.rb
+++ lib/CLIproject/tequila_drink.rb
@@ -3,9 +3,7 @@ class TequilaDrink
     @@all=[]
 
     def initialize(hash)
-        # @strDrink = name
-        # @idDrink = id
-        
+    
        hash.each do |k,v|
             self.send("#{k}=", v) if self.respond_to?(k)
        end
