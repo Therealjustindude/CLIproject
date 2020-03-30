7184e06813379057099a50757128f7bca6f46377
therealjustindude <therealjustindude@gmail.com>
2020-03-30 11:31:23 -0500

    CLI is handling most invalid entries

--- lib/CLIproject/cli.rb
+++ lib/CLIproject/cli.rb
@@ -2,31 +2,87 @@ class CLI
     def start
         API.tequila_drinks
         self.welcome
-
-       
     end
 
     def welcome
         
         puts "We have a list of tequila drinks"
         puts "We will supply a shopping list and instructions"
+            sleep (2)
         puts "Enter the number that interests you!"
+            sleep (2)
+
         TequilaDrink.list_drink_names
-        @input = gets.chomp.to_i 
-        find_obj_w_input
+         @input = gets.chomp.to_i 
+
+            if @input.between?(1,24)
+                find_obj_w_input
+            else
+             welcome
+            end
+
         puts "Here is your shopping list:"
-        get_shopping_list
-        puts "Enter 'instructions' too see them or 'exit' to go shopping"
-        input = gets.chomp
-        # if input.downcase  == "instructions"
-        #    get_instructions
-        # elsif input.downcase == "exit"
-        #     puts "Tequila may not be the answer but it's worth a shot!" 
-        # else
-        #     puts "Maybe it's the Tequila but that is not a valid response. Please try again."
-        # end
-        # welcome
+            get_shopping_list
+            sleep (0.5)
+
+        puts "Enter 'instructions' to see them, 'list' for Tequila drinks or 'exit' to get going"
         
+            @input2 = gets.chomp.downcase 
+            test_input(@input2)
+
+        puts "Would you like to go to 'instructions', 'list' or 'exit'"
+            @input3= gets.chomp.downcase
+            options(@input3)
+
+       
+        
+    end
+
+    def options(input)
+        if input == 'instructions'
+            get_instructions
+            list_or_exit
+        elsif input == 'list'
+            welcome
+        else
+            exit
+        end
+    end
+
+    def list_or_exit
+        puts "Would you like to go to 'list' or 'exit'"
+        input= gets.chomp.downcase
+        if input == 'list'
+            welcome
+            sleep (0.5)
+        elsif input == 'exit'
+            exit
+        else
+            invalid_entry
+            sleep (0.5)
+            list_or_exit
+        end
+    end
+
+    def test_input(input)
+        if input == "instructions"
+            get_instructions
+            sleep (1)
+            list_or_exit
+        elsif input == "list"
+            welcome
+            sleep (0.5)
+        elsif input == "exit"
+            exit
+        else
+            invalid_entry
+            sleep (0.5)
+            #need to go back to options some how
+        end
+    end
+
+    def invalid_entry
+        puts "invalid. try again."
     end
 
     def find_obj_w_input
