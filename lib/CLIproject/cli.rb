5fbd59f476153e76b8b2d4f95d34e9518fcb89e0
therealjustindude <therealjustindude@gmail.com>
2020-03-30 12:40:11 -0500

    CLI is handling invalid entry at all points

--- lib/CLIproject/cli.rb
+++ lib/CLIproject/cli.rb
@@ -1,88 +1,77 @@
 class CLI
     def start
         API.tequila_drinks
-        self.welcome
+        welcome_message
     end
 
-    def welcome
+    def welcome_message
         
         puts "We have a list of tequila drinks"
         puts "We will supply a shopping list and instructions"
-            sleep (2)
+            sleep (1.5)
         puts "Enter the number that interests you!"
             sleep (2)
 
+        start_and_list_drinks
+    end
+
+    def start_and_list_drinks
         TequilaDrink.list_drink_names
-         @input = gets.chomp.to_i 
-
-            if @input.between?(1,24)
-                find_obj_w_input
-            else
-             welcome
-            end
-
-        puts "Here is your shopping list:"
-            get_shopping_list
-            sleep (0.5)
-
-        puts "Enter 'instructions' to see them, 'list' for Tequila drinks or 'exit' to get going"
-        
-            @input2 = gets.chomp.downcase 
-            test_input(@input2)
-
-        puts "Would you like to go to 'instructions', 'list' or 'exit'"
-            @input3= gets.chomp.downcase
-            options(@input3)
-
-       
-        
+        test_user_input
+        get_shopping_list
+        whats_next
     end
-
-    def options(input)
-        if input == 'instructions'
-            get_instructions
-            list_or_exit
-        elsif input == 'list'
-            welcome
-        else
-            exit
-        end
-    end
-
-    def list_or_exit
-        puts "Would you like to go to 'list' or 'exit'"
-        input= gets.chomp.downcase
-        if input == 'list'
-            welcome
-            sleep (0.5)
-        elsif input == 'exit'
-            exit
+    
+    def test_user_input #tests user drink selection
+        @input = gets.chomp.to_i
+        if @input.between?(1,24)
+             find_obj_w_input
         else
             invalid_entry
-            sleep (0.5)
             list_or_exit
         end
     end
 
-    def test_input(input)
+    def whats_next 
+        puts "Enter 'instructions' to see them, 'list' to see Tequila drinks again or 'exit' to get going"
+            @input2 = gets.chomp.downcase 
+            next_input_test(@input2)
+    end
+
+    def next_input_test(input) #tests users input for whats next 
         if input == "instructions"
             get_instructions
             sleep (1)
             list_or_exit
         elsif input == "list"
-            welcome
+            start_and_list_drinks
             sleep (0.5)
         elsif input == "exit"
             exit
         else
             invalid_entry
+            whats_next
+        end
+    end
+   
+    def list_or_exit
+        puts "Would you like to go to the 'list' of tequila drinks or 'exit'"
+        puts "Please enter 'list' or 'exit'"
+        input= gets.chomp.downcase
+        if input == 'list'
+            start_and_list_drinks
             sleep (0.5)
-            #need to go back to options some how
+        elsif input == 'exit'
+            exit
+        else
+            invalid_entry
+            list_or_exit
         end
     end
 
     def invalid_entry
-        puts "invalid. try again."
+        puts "Maybe it's the Tequila but im not sure what you're asking. Please try again."
+        sleep (1.5)
     end
 
     def find_obj_w_input
@@ -96,6 +85,7 @@ class CLI
     end
 
     def get_shopping_list
+        puts "Here is your shopping list:"
         tequila_obj=TequilaDrink.all[@input - 1]
         array = tequila_obj.strIngredients
         array.each.with_index(1) do |ingredient, idx|
